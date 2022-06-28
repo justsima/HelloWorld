498282253d43348ec063121526394bd794134979
Simon <asayesimon123@gmail.com>
2022-06-28 14:17:32 +0300

    Lab 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿/*using System;
 
 namespace ConsoleApp1
 {
+    class Student {
+        public int Id;
+        public string Name;
+     }
+
     class program
     {
         static void Main(string[] args)
         {
+            Student st = new ();
+            st.Id = 10;
             int num1, num2;
             num1 = 21;
             num2 = 22;
@@ -18,10 +25,44 @@ namespace ConsoleApp1
             Console.WriteLine("Hello " + str);
             Console.WriteLine("From GitHub web! ");
             Console.WriteLine("From PC in the BEST laptop");
+            Console.WriteLine("this is anoither trail ");
             Console.Read();
-
 
         }
 
+    }
+}
+*/
+using System;
+namespace ConSoleApp1
+{
+    class student
+    {
+        private int _id;
+        private string _name;
+        public int Id
+        {
+            get => _id; 
+            set => _id = value; 
+        }
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+    }
+    
+    class program
+    {
+        static void Main(string[] args)
+        {
+            student st = new student();
+            st.Id = 20;
+            st.Name = "Simon";
+            Console.WriteLine("Name is " + st.Name);
+            Console.WriteLine("Age is "+ st.Id);
+            
+        }
     }
 }
