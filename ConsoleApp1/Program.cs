304f87cc9faddfc1948bb2f7e2c9af0c6a7a2af4
Simon <asayesimon123@gmail.com>
2022-07-05 08:20:53 +0300

    update on Functions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,46 +1,11 @@
-﻿/*using System;
-
-namespace ConsoleApp1
-{
-    class Student {
-        public int Id;
-        public string Name;
-     }
-
-    class program
-    {
-        static void Main(string[] args)
-        {
-            Student st = new ();
-            st.Id = 10;
-            int num1, num2;
-            num1 = 21;
-            num2 = 22;
-            string str = "Simon";
-
-            Console.WriteLine("Num1 = " + num1 + "\nNum2 = " + num2);
-            int sum = num1 + num2;
-            Console.WriteLine("Sum = " + sum);
-            Console.WriteLine();
-            Console.WriteLine("Hello " + str);
-            Console.WriteLine("From GitHub web! ");
-            Console.WriteLine("From PC in the BEST laptop");
-            Console.WriteLine("this is anoither trail ");
-            Console.Read();
-
-        }
-
-    }
-}
-*/
-using System;
+﻿using System;
 namespace ConSoleApp1
 {
     class student
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; };
+        public string Email { get; set; }
     }
     
     class program : student
---
+++ Functions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    abstract class shape
+    {
+        public abstract double calcArea();
+        public abstract double calcPermiter();
+    }
+   class Circle : shape 
+    {
+        public double radius { get; set; }
+        static readonly double PI = 3.14;
+        public override double calcArea()
+        {
+            return PI * radius * radius;
+        }
+        public override double calcPermiter()
+        {
+            return 2*PI*radius;
+        }
+
+    }
+    class Rectangle : shape
+    {
+        public double height { get; set; }
+        public double length { get; set; }
+    public override double calcArea()
+        {
+            return height*length;
+        }
+    public override double calcPermiter()
+        {
+            return 2 * (length * height);                ;
+        }
+} 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle cr = new Circle();
+                   cr.calcArea();
+        }
+    }
+}
