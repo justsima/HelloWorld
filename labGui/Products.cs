fdfb4709c054e5b828ff6b44e93ae75519dd12d3
Simon <asayesimon123@gmail.com>
2022-07-05 15:12:11 +0300

    Lab GUI

---
+++ labGui/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labGui
+{
+    internal class Products
+    {
+		public int number { get; set; }
+		public DateTime date { get; set; }
+		public int inventory { get; set; }
+		public string name { get; set; }
+		public int count { get; set; }
+		public int price { get; set; }
+		public void save()
+		{
+			Console.WriteLine("Query saved ");
+		}
+	}
+}
